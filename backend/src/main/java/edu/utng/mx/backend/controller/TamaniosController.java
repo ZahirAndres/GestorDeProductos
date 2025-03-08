c967bd197f2e1b920abe1828899fc4670c94d2c1
ZahirAndres <rodriguez.mora.zahir.15@gmail.com>
2025-03-07 23:23:38 -0600

    ahhhh

--- backend/src/main/java/edu/utng/mx/backend/controller/TamaniosController.java
+++ backend/src/main/java/edu/utng/mx/backend/controller/TamaniosController.java
@@ -5,13 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import edu.utng.mx.backend.repository.ProveedorRepository;
 import edu.utng.mx.backend.repository.TamanioRepository;
 import edu.utng.mx.backend.documentos.Tamanio;
-import edu.utng.mx.backend.documentos.Producto;
 
 @RestController
 @RequestMapping("/api/formularios/tamanios")
